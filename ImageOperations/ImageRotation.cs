770e7c2794f017e724275cdf4cbd81c97df948b6
rgyang <rgyang@ctrip.com>
2014-02-19 18:08:56 +0800

    update 20140219

---
+++ ImageOperations/ImageRotation.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class ImageRotation
+{
+    public Bitmap DoImageRotationCertainAngle(double angle, string path)
+    {
+        Bitmap source = GetImage2Bitmap(path);
+        return DoImageRotationCertainAngle(angle, source);
+    }
+
+    public Bitmap DoImageRotationCertainAngle(double angle, Bitmap source)
+    {
+        int iw = 0, ih = 0;
+        int sW = 0, sH = 0;
+        int nW = 0, nH = 0;
+        int rposH = 0, rposW = 0;
+        int i = 0, m = 0, n = 0;
+        int leftH = 0, leftW = 0;
+        int rightH = 0, rightW = 0;
+        int tR = 0, tG = 0, tB = 0;
+        int cur = 0, pos = 0, mask = 255;
+
+        int[] validKey = new int[9];
+        int[] validValue = new int[9];
+
+        double C1 = 0.0, C2 = 0.0;
+        double D1 = 0.0, D2 = 0.0;
+        double correct = 0.5, half = 0.5;
+        double sinA = 0.0, cosA = 0.0;
+
+        while (angle < 0) { angle += 360; }
+
+        angle %= 360;
+        if (angle > 180) { angle = angle - 360; }
+
+        double radian = TranslateAngleToRadian(angle);
+        sinA = Math.Sin(radian);
+        cosA = Math.Cos(radian);
+
+        sW = source.Width;
+        sH = source.Height;
+
+        nW = Convert.ToInt32(correct + Math.Abs(sH * sinA) + Math.Abs(sW * cosA));
+        nH = Convert.ToInt32(correct + Math.Abs(sH * cosA) + Math.Abs(sW * sinA));
+
+        C1 = ((nH + correct) - (sH + correct) * cosA - (sW + correct) * sinA) * half;
+        C2 = ((nW + correct) + (sH + correct) * sinA - (sW + correct) * cosA) * half;
+
+        D1 = -C1 * cosA + C2 * sinA;
+        D2 = -C1 * sinA - C2 * cosA;
+
+        while (nW % 4 != 0) { ++nW; }
+
+        Bitmap destin = new Bitmap(nW, nH);
+
+        BitmapData sourceData = source.LockBits(new Rectangle(0, 0, sW, sH), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+        BitmapData destinData = destin.LockBits(new Rectangle(0, 0, nW, nH), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+        unsafe
+        {
+            byte* sourcePtr = (byte*)sourceData.Scan0;
+            byte* destinPtr = (byte*)destinData.Scan0;
+
+            for (ih = 0; ih < nH; ++ih)
+            {
+                for (iw = 0; iw < nW; ++iw)
+                {
+                    cur = 0;
+                    pos = 0;
+
+                    rposH = Convert.ToInt32(ih * cosA - iw * sinA + D1);
+                    rposW = Convert.ToInt32(ih * sinA + iw * cosA + D2);
+
+                    for (i = 0; i < 9; validKey[i] = validValue[i] = -1, ++i) ;
+
+                    for (m = rposH - 1; m <= rposH + 1; ++m)
+                    {
+                        if (m >= 0 && m < sH)
+                        {
+                            for (n = rposW - 1; n <= rposW + 1; ++n)
+                            {
+                                if (n >= 0 && n < sW)
+                                {
+                                    cur = m * sourceData.Stride + n * 3;
+                                    tR = sourcePtr[cur + 0];
+                                    tG = sourcePtr[cur + 1];
+                                    tB = sourcePtr[cur + 2];
+                                    cur = (tR << 16) | (tG << 8) | tB;
+                                    for (i = 0; i < 9; ++i)
+                                    {
+                                        if (validKey[i] == cur)
+                                        {
+                                            validValue[i] += 1;
+                                            break;
+                                        }
+                                    }
+                                    if (i == 9)
+                                    {
+                                        validKey[pos] = cur;
+                                        validValue[pos] = 1;
+                                        pos += 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    cur = 0;
+                    m = 16777215;
+
+                    for (i = 0; i < pos; ++i)
+                    {
+                        if (validValue[i] > cur)
+                        {
+                            m = validKey[i];
+                            cur = validValue[i];
+                        }
+                    }
+                    cur = ih * destinData.Stride + iw * 3;
+                    destinPtr[cur + 0] = (byte)(m >> 16);
+                    destinPtr[cur + 1] = (byte)((m >> 8) & mask);
+                    destinPtr[cur + 2] = (byte)(m & mask);
+                }
+            }
+            source.UnlockBits(sourceData);
+            destin.UnlockBits(destinData);
+        }
+
+        GC.Collect(2);
+
+        return destin;
+    }
+
+    public Bitmap GetImage2Bitmap(string path)
+    {
+        if (!File.Exists(path))
+        {
+            throw new Exception("File is not exist.");
+        }
+
+        Bitmap source = new Bitmap(path);
+
+        return source;
+    }
+
+    public double TranslateAngleToRadian(double angle)
+    {
+        return Math.PI * angle / 180.0;
+    }
+}
---
+++ ImageOperations/Tailor.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class Tailor
+{
+    /// <summary>
+    /// Using threshold to generate templates.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static Bitmap Using_Threshold_To_Generate_Templates(Bitmap source)
+    {
+        byte v = 255;
+        int i = 0, j = 0, k = 0;
+        int cur = 0, ncur = 0, avg = 0;
+        Bitmap destin = null;
+        int nw = 0, nh = 0;
+        int w = source.Width, h = source.Height;
+        int leftX = int.MaxValue, leftY = int.MaxValue;
+        int rightX = int.MinValue, rightY = int.MinValue;
+
+        BitmapData sourceData = source.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+        unsafe
+        {
+            byte* sourcePtr = (byte*)sourceData.Scan0;
+            for (i = 0; i < h; ++i)
+            {
+                for (j = 0; j < w; ++j)
+                {
+                    v = 255;
+                    cur = i * sourceData.Stride + j * 3;
+                    avg = Convert.ToInt32((
+                        Convert.ToInt32(sourcePtr[cur + 0]) +
+                        Convert.ToInt32(sourcePtr[cur + 1]) +
+                        Convert.ToInt32(sourcePtr[cur + 2])) / 3);
+
+                    if (avg == 255) { continue; }
+
+                    if (avg != Config.ThresHold)
+                    {
+                        v = 0;
+                        if (i < leftX) { leftX = i; }
+                        if (i > rightX) { rightX = i; }
+                        if (j < leftY) { leftY = j; }
+                        if (j > rightY) { rightY = j; }
+                    }
+                    for (k = 0; k < 3; ++k)
+                    {
+                        sourcePtr[cur + k] = v;
+                    }
+                }
+            }
+
+            nw = rightY - leftY + 1;
+            nh = rightX - leftX + 1;
+
+            destin = new Bitmap(nw, nh);
+            BitmapData destinData = destin.LockBits(new Rectangle(0, 0, nw, nh), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte* destinPtr = (byte*)destinData.Scan0;
+
+            for (i = 0; i < nh; ++i)
+            {
+                for (j = 0; j < nw; ++j)
+                {
+                    cur = (i + leftX) * sourceData.Stride + (j + leftY) * 3;
+                    ncur = i * destinData.Stride + j * 3;
+                    for (k = 0; k < 3; ++k)
+                    {
+                        destinPtr[ncur + k] = sourcePtr[cur + k];
+                    }
+                }
+            }
+
+            source.UnlockBits(sourceData);
+            destin.UnlockBits(destinData);
+
+            source.Dispose();
+        }
+
+        return destin;
+    }
+
+    public static Bitmap Using_Threshold_To_Generate_Templates(string path)
+    {
+        Bitmap source = Operations.Convert_Jpg2Bmp(path);
+        return Using_Threshold_To_Generate_Templates(source);
+    }
+
+    public static void FindBlackRegionSize(Bitmap source, ref int leftH, ref int leftW, ref int rightH, ref int rightW)
+    {
+        int i = 0, j = 0;
+        int w = source.Width;
+        int h = source.Height;
+
+        BitmapData sourceData = source.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+        leftH = leftW = int.MaxValue;
+        rightH = rightW = int.MinValue;
+        unsafe
+        {
+            byte* sourcePtr = (byte*)sourceData.Scan0;
+
+            for (i = 0; i < h; ++i)
+            {
+                for (j = 0; j < w; ++j)
+                {
+                    if (sourcePtr[i * sourceData.Stride + j * 3] == 0)
+                    {
+                        if (i < leftH) { leftH = i; }
+                        if (i > rightH) { rightH = i; }
+                        if (j < leftW) { leftW = j; }
+                        if (j > rightW) { rightW = j; }
+                    }
+                }
+            }
+
+            source.UnlockBits(sourceData);
+        }
+        GC.Collect(0);
+    }
+
+    public static Bitmap ImageTailor(Bitmap source, int leftH, int leftW, int rightH, int rightW)
+    {
+        int i = 0, j = 0, k = 0;
+        int h = rightH - leftH + 1;
+        int w = rightW - leftW + 1;
+
+        Bitmap tailor = new Bitmap(w, h);
+
+        BitmapData sourceData = source.LockBits(new Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+        BitmapData tailorData = tailor.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+        unsafe
+        {
+            byte* sourcePtr = (byte*)sourceData.Scan0;
+            byte* tailorPtr = (byte*)tailorData.Scan0;
+
+            for (i = 0; i < h; ++i)
+            {
+                for (j = 0; j < w; ++j)
+                {
+                    int sCur = (i + leftH) * sourceData.Stride + (j + leftW) * 3;
+                    int nCur = i * tailorData.Stride + j * 3;
+
+                    for (k = 0; k < 3; ++k)
+                    {
+                        tailorPtr[nCur + k] = sourcePtr[sCur + k];
+                    }
+                }
+            }
+
+            source.UnlockBits(sourceData);
+            tailor.UnlockBits(tailorData);
+
+            source.Dispose();
+        }
+
+        GC.Collect(2);
+
+        return tailor;
+    }
+}
